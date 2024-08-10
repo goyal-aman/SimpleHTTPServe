8e8dba231abab7b3881912004f23cd3f99bf1c13
Aman Goyal <47478778+goyal-aman@users.noreply.github.com>
2024-08-10 19:51:54 +0530

    add place holder tests

---
+++ tests/app_tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+# Place holder for how
+class TestSimpleHTTPServer(unittest.TestCase):
+
+    def setUp(self):
+        # This method will be run before each test.
+        # You can initialize your app or any test dependencies here.
+        pass
+
+    def test_placeholder(self):
+        # This is a placeholder test method.
+        # It currently does nothing but passes.
+        self.assertTrue(True, "This is a placeholder test")
+
+if __name__ == "__main__":
+    unittest.main()
